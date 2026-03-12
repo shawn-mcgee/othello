a16492ec81a7f840a52c9dfb858245e9149f8d3b
shawn-mcgee <s.mcgeek07@gmail.com>
2026-03-12 08:10:29 -0600

    feat: rename project to Othello and update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/go',
+  base: '/othello/',
   plugins: [react(), tailwindcss()],
 })
